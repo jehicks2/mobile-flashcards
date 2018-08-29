2dce84ade63ab27d14d1f4011aa934b69e7b490e
James Hicks <jehicks2@yahoo.com>
2018-08-28 22:55:25 -0600

    changed notification to every 24 hrs

--- components/Notification.js
+++ components/Notification.js
@@ -45,7 +45,7 @@ export function setLocalNotification () {
 
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
-              tomorrow.setHours(20)
+              tomorrow.setHours(24)
               tomorrow.setMinutes(0)
 
               Notifications.scheduleLocalNotificationAsync(
